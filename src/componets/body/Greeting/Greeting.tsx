a233af1c43bb40f591ee03c4ca271c8f08fdc529
Ivan Pasternak <paster-ivan@yandex.by>
2022-05-26 18:01:03 +0300

    refactor code add Skill

--- src/componets/body/Greeting/Greeting.tsx
+++ src/componets/body/Greeting/Greeting.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import style from './Greeting.module.css'
+import containerStyle from './../../../common/styles/Container.module.css'
 
 export const Greeting = () => {
     return (
         <div className={style.greeting}>
+            <div className={containerStyle.container}>
+                <div className={style.text}>
+                    <span>Hi there</span>
+                    <h1>I am Ivan</h1>
+                    <p>React Developer</p>
+                </div>
+                <div className={style.photo}>
 
+                </div>
+            </div>
         </div>
     )
 }
--- src/componets/body/Skills/Skills.tsx
+++ src/componets/body/Skills/Skills.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import style from './Skills.module.css'
+import containerStyle from './../../../common/styles/Container.module.css'
+import {Skill} from "./Skill";
 
 export const Skills = () => {
     return (
         <div className={style.skills}>
-
+            <div className={containerStyle.container}>
+                <Skill />
+            </div>
         </div>
     )
 }
